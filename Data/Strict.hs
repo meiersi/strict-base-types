5b1560b5f5439e522792d23a3f31069a7939529f
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-03-20 14:12:31 +0200

    Remove Hyperstrict
    darcs-hash:20070320121231-b2b0a-0ab84e0085b4cc35502b0a83811374e5efe8dcb6

--- Data/Strict.hs
+++ Data/Strict.hs
@@ -17,12 +17,10 @@ module Data.Strict (
   , module Data.Strict.Maybe
   , module Data.Strict.Either
   , module Data.Strict.Lazy
-  , module Data.Strict.Hyperstrict
 ) where
 
 import Data.Strict.Tuple
 import Data.Strict.Maybe
 import Data.Strict.Either
 import Data.Strict.Lazy
-import Data.Strict.Hyperstrict
 
