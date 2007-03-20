f1fa115e4a7d83e4c9f1386bf6d1f7c1a74dcbcb
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-03-20 17:06:45 +0200

    Remove Data.Lazy and SSeq
    darcs-hash:20070320150645-b2b0a-ab84fc8a7ce6dade1adb431735e270d5f9072b6e

--- Data/Strict.hs
+++ Data/Strict.hs
@@ -16,49 +16,9 @@ module Data.Strict (
     module Data.Strict.Tuple
   , module Data.Strict.Maybe
   , module Data.Strict.Either
-
-  , SSeq(..)
-  , Hide(..)
 ) where
 
 import Data.Strict.Tuple
-import Data.Strict.Tuple  as S
 import Data.Strict.Maybe
-import Data.Strict.Maybe  as S
 import Data.Strict.Either
-import Data.Strict.Either as S
-
-import Data.Lazy
-
-infixr 0 `sseq`
-
-class SSeq a where
-  sseq :: a -> b -> b
-
-  {-# INLINE sseq #-}
-  sseq = seq
-
-instance SSeq ()
-instance SSeq Bool
-instance SSeq Char
-instance SSeq Int
-instance SSeq Float
-instance SSeq Double
-
-instance (SSeq a, SSeq b) => SSeq (S.Pair a b) where
-  x S.:!: y `sseq` z = x `sseq` y `sseq` z
-
-instance SSeq a => SSeq (S.Maybe a) where
-  S.Nothing `sseq` y = y
-  S.Just x  `sseq` y = x `sseq` y
-
-instance (SSeq a, SSeq b) => SSeq (S.Either a b) where
-  S.Left  x `sseq` y = x `sseq` y
-  S.Right x `sseq` y = x `sseq` y
-
-newtype Hide a = Hide a
-
-instance SSeq (Hide a)
-
-instance SSeq (Lazy a)
 
