e8369ac560e27db839e1a4553cbf118c7d4d03f5
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-03-20 16:32:01 +0200

    Add Strict class
    darcs-hash:20070320143201-b2b0a-6c2d0dd256e5d9f289dba6457f36edd16b5bf33d

--- Data/Strict.hs
+++ Data/Strict.hs
@@ -19,6 +19,35 @@ module Data.Strict (
 ) where
 
 import Data.Strict.Tuple
+import Data.Strict.Tuple  as S
 import Data.Strict.Maybe
+import Data.Strict.Maybe  as S
 import Data.Strict.Either
+import Data.Strict.Either as S
+
+infixr 0 `sseq`
+
+class Strict a where
+  sseq :: a -> b -> b
+
+  {-# INLINE sseq #-}
+  sseq = seq
+
+instance Strict ()
+instance Strict Bool
+instance Strict Char
+instance Strict Int
+instance Strict Float
+instance Strict Double
+
+instance (Strict a, Strict b) => Strict (S.Pair a b) where
+  x S.:!: y `sseq` z = x `sseq` y `sseq` z
+
+instance Strict a => Strict (S.Maybe a) where
+  S.Nothing `sseq` y = y
+  S.Just x  `sseq` y = x `sseq` y
+
+instance (Strict a, Strict b) => Strict (S.Either a b) where
+  S.Left  x `sseq` y = x `sseq` y
+  S.Right x `sseq` y = x `sseq` y
 
