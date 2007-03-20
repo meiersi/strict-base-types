45f3bce5df10781761ff8b663960c9bba4ba071c
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-03-20 14:13:24 +0200

    Remove Lazy
    darcs-hash:20070320121324-b2b0a-a7d17f84564f0aa4710bc822d153fd330fbfc623

--- Data/Strict.hs
+++ Data/Strict.hs
@@ -16,11 +16,9 @@ module Data.Strict (
     module Data.Strict.Tuple
   , module Data.Strict.Maybe
   , module Data.Strict.Either
-  , module Data.Strict.Lazy
 ) where
 
 import Data.Strict.Tuple
 import Data.Strict.Maybe
 import Data.Strict.Either
-import Data.Strict.Lazy
 
